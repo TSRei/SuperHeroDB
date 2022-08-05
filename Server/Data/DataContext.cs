930218819cf1c487f6ba85d8a6bf02e78f16d008
TSL-coding <lowder.trevor@yahoo.com>
2022-08-04 19:14:49 -0700

    Add project files.

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,37 @@
+﻿
+namespace BlazorFullStackCRUD.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Comic>().HasData(
+                new Comic { Id = 1, Name = "Marvel" }, new Comic { Id = 2, Name = "DC" });
+            modelbuilder.Entity<SuperHero>().HasData(
+                new SuperHero
+                {
+                    Id = 1,
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    HeroName = "Spider-Man",
+                    ComicId = 1,
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    HeroName = "Batman",
+                    ComicId = 2,
+                });
+        }
+
+        public DbSet<Comic> Comics { get; set; }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
